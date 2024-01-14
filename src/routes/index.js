cdf1249fe9f40c49950562220ca62cf789e5025d
gustagod <gustavoalain93@outlook.com>
2024-01-13 19:20:24 -0700

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userRoute=require('./user.route')
 
-
-// colocar las rutas aquí
-router.use(userRoute)
+router.use(userRoute);
 
 module.exports = router;
